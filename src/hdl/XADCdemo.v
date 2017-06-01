8ae2459e019f9aeeb8557bf67533c7295ab6cf85
Arthur Brown <arthur.brown@email.wsu.edu>
2017-06-01 10:28:22 -0700

    Fixed timing slack warnings
    The massive repeated division process in XADCDemo.v was causing various
    warnings. This block has been replaced with a simple state machine
    (modified from the Nexys Video XADC repo)

--- src/hdl/XADCdemo.v
+++ src/hdl/XADCdemo.v
@@ -14,177 +14,159 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
+// Revision 0.02 - Fixed timing slack (ArtVVB 06/01/17)
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
  
 
 module XADCdemo(
-   input CLK100MHZ,
-   input vauxp6,
-   input vauxn6,
-   input vauxp7,
-   input vauxn7,
-   input vauxp15,
-   input vauxn15,
-   input vauxp14,
-   input vauxn14,
-   input vp_in,
-   input vn_in,
-   input [1:0] sw,
-   output reg [15:0] LED,
-   output [3:0] an,
-   output dp,
-   output [6:0] seg 
- );
-   
-   wire enable;  
-   wire ready;
-   wire [15:0] data;   
-   reg [6:0] Address_in;     
-   reg [32:0] decimal;   
-   reg [3:0] dig0;
-   reg [3:0] dig1;
-   reg [3:0] dig2;
-   reg [3:0] dig3;
-   reg [3:0] dig4;
-   reg [3:0] dig5;
-   reg [3:0] dig6;
+    input CLK100MHZ,
+    input vauxp6,
+    input vauxn6,
+    input vauxp7,
+    input vauxn7,
+    input vauxp15,
+    input vauxn15,
+    input vauxp14,
+    input vauxn14,
+    input vp_in,
+    input vn_in,
+    input [1:0] sw,
+    output reg [15:0] led,
+    output [3:0] an,
+    output dp,
+    output [6:0] seg
+);
 
-  
-  
-   
-   //xadc instantiation connect the eoc_out .den_in to get continuous conversion
-   xadc_wiz_0  XLXI_7 (.daddr_in(Address_in), //addresses can be found in the artix 7 XADC user guide DRP register space
-                     .dclk_in(CLK100MHZ), 
-                     .den_in(enable), 
-                     .di_in(0), 
-                     .dwe_in(0), 
-                     .busy_out(),                    
-                     .vauxp6(vauxp6),
-                     .vauxn6(vauxn6),
-                     .vauxp7(vauxp7),
-                     .vauxn7(vauxn7),
-                     .vauxp14(vauxp14),
-                     .vauxn14(vauxn14),
-                     .vauxp15(vauxp15),
-                     .vauxn15(vauxn15),
-                     .vn_in(vn_in), 
-                     .vp_in(vp_in), 
-                     .alarm_out(), 
-                     .do_out(data), 
-                     //.reset_in(),
-                     .eoc_out(enable),
-                     .channel_out(),
-                     .drdy_out(ready));
-                     
-         
+    wire enable;  
+    wire ready;
+    wire [15:0] data;   
+    reg [6:0] Address_in;
+	
+	//secen segment controller signals
+    reg [32:0] count;
+    localparam S_IDLE = 0;
+    localparam S_FRAME_WAIT = 1;
+    localparam S_CONVERSION = 2;
+    reg [1:0] state = S_IDLE;
+    reg [15:0] sseg_data;
+	
+	//binary to decimal converter signals
+    reg b2d_start;
+    reg [15:0] b2d_din;
+    wire [15:0] b2d_dout;
+    wire b2d_done;
+
+    //xadc instantiation connect the eoc_out .den_in to get continuous conversion
+    xadc_wiz_0  XLXI_7 (
+        .daddr_in(Address_in), //addresses can be found in the artix 7 XADC user guide DRP register space
+        .dclk_in(CLK100MHZ), 
+        .den_in(enable), 
+        .di_in(0), 
+        .dwe_in(0), 
+        .busy_out(),                    
+        .vauxp6(vauxp6),
+        .vauxn6(vauxn6),
+        .vauxp7(vauxp7),
+        .vauxn7(vauxn7),
+        .vauxp14(vauxp14),
+        .vauxn14(vauxn14),
+        .vauxp15(vauxp15),
+        .vauxn15(vauxn15),
+        .vn_in(vn_in), 
+        .vp_in(vp_in), 
+        .alarm_out(), 
+        .do_out(data), 
+        //.reset_in(),
+        .eoc_out(enable),
+        .channel_out(),
+        .drdy_out(ready)
+    );
     
-      //led visual dmm              
-      always @( posedge(CLK100MHZ))
-      begin            
-        if(ready == 1'b1)
-        begin
-          case (data[15:12])
-            1:  LED <= 16'b11;
-            2:  LED <= 16'b111;
-            3:  LED <= 16'b1111;
-            4:  LED <= 16'b11111;
-            5:  LED <= 16'b111111;
-            6:  LED <= 16'b1111111; 
-            7:  LED <= 16'b11111111;
-            8:  LED <= 16'b111111111;
-            9:  LED <= 16'b1111111111;
-            10: LED <= 16'b11111111111;
-            11: LED <= 16'b111111111111;
-            12: LED <= 16'b1111111111111;
-            13: LED <= 16'b11111111111111;
-            14: LED <= 16'b111111111111111;
-            15: LED <= 16'b1111111111111111;                        
-           default: LED <= 16'b1; 
-           endcase
-        end 
-
-          
-      end
-      
-     reg [32:0] count; 
-     //binary to decimal conversion
-      always @ (posedge(CLK100MHZ))
-      begin
-      
-        if(count == 10000000)begin
-        
-        decimal = data >> 4;
-        //looks nicer if our max value is 1V instead of .999755
-        if(decimal >= 4093)
-        begin
-            dig0 = 0;
-            dig1 = 0;
-            dig2 = 0;
-            dig3 = 0;
-            dig4 = 0;
-            dig5 = 0;
-            dig6 = 1;
-            count = 0;
+    //led visual dmm              
+    always @(posedge(CLK100MHZ)) begin            
+        if(ready == 1'b1) begin
+            case (data[15:12])
+            1:  led <= 16'b11;
+            2:  led <= 16'b111;
+            3:  led <= 16'b1111;
+            4:  led <= 16'b11111;
+            5:  led <= 16'b111111;
+            6:  led <= 16'b1111111; 
+            7:  led <= 16'b11111111;
+            8:  led <= 16'b111111111;
+            9:  led <= 16'b1111111111;
+            10: led <= 16'b11111111111;
+            11: led <= 16'b111111111111;
+            12: led <= 16'b1111111111111;
+            13: led <= 16'b11111111111111;
+            14: led <= 16'b111111111111111;
+            15: led <= 16'b1111111111111111;                        
+            default: led <= 16'b1; 
+            endcase
         end
-        else 
-        begin
-            decimal = decimal * 250000;
-            decimal = decimal >> 10;
-            
-            
-            dig0 = decimal % 10;
-            decimal = decimal / 10;
-            
-            dig1 = decimal % 10;
-            decimal = decimal / 10;
-                   
-            dig2 = decimal % 10;
-            decimal = decimal / 10;
-            
-            dig3 = decimal % 10;
-            decimal = decimal / 10;
-            
-            dig4 = decimal % 10;
-            decimal = decimal / 10;
-                   
-            dig5 = decimal % 10;
-            decimal = decimal / 10; 
-            
-            dig6 = decimal % 10;
-            decimal = decimal / 10; 
-            
-            count = 0;
+    end
+    
+    //binary to decimal conversion
+    always @ (posedge(CLK100MHZ)) begin
+        case (state)
+        S_IDLE: begin
+            state <= S_FRAME_WAIT;
+            count <= 'b0;
         end
-       end
-       
-      count = count + 1;
-               
-      end
+        S_FRAME_WAIT: begin
+            if (count >= 10000000) begin
+                if (data > 16'hFFD0) begin
+                    sseg_data <= 16'h1000;
+                    state <= S_IDLE;
+                end else begin
+                    b2d_start <= 1'b1;
+                    b2d_din <= data;
+                    state <= S_CONVERSION;
+                end
+            end else
+                count <= count + 1'b1;
+        end
+        S_CONVERSION: begin
+            b2d_start <= 1'b0;
+            if (b2d_done == 1'b1) begin
+                sseg_data <= b2d_dout;
+                state <= S_IDLE;
+            end
+        end
+        endcase
+    end
+    
+    bin2dec m_b2d (
+        .clk(CLK100MHZ),
+        .start(b2d_start),
+        .din(b2d_din),
+        .done(b2d_done),
+        .dout(b2d_dout)
+    );
       
-      always @(posedge(CLK100MHZ))
-      begin
+    always @(posedge(CLK100MHZ)) begin
         case(sw)
         0: Address_in <= 8'h16;
         1: Address_in <= 8'h17;
         2: Address_in <= 8'h1e;
         3: Address_in <= 8'h1f;
         endcase
-      
-      
-      end
-      
-      DigitToSeg segment1(.in1(dig3),
-                         .in2(dig4),
-                         .in3(dig5),
-                         .in4(dig6),
-                         .in5(),
-                         .in6(),
-                         .in7(),
-                         .in8(),
-                         .mclk(CLK100MHZ),
-                         .an(an),
-                         .dp(dp),
-                         .seg(seg));  
+    end
+    
+    DigitToSeg segment1(
+        .in1(sseg_data[3:0]),
+        .in2(sseg_data[7:4]),
+        .in3(sseg_data[11:8]),
+        .in4(sseg_data[15:12]),
+        .in5(),
+        .in6(),
+        .in7(),
+        .in8(),
+        .mclk(CLK100MHZ),
+        .an(an),
+        .dp(dp),
+        .seg(seg)
+    );
 endmodule
